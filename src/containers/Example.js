faa9ebb5a93e03e8f53433cbbc1a702b6b119201
kopaka7 <Naren@lawn-128-61-20-178.lawn.gatech.edu>
2018-02-09 14:02:53 -0500

    Fixed redux connection issue

--- src/containers/Example.js
+++ src/containers/Example.js
@@ -15,24 +15,6 @@ class Example extends Component {
 	isFloopMajor(user) {
 		return user.major === "Floopanomics";
 	}
-
-<<<<<<< HEAD
-	renderInterests(interests) {
-		return interests.map((entry, index) =>
-			(<div className="interest" key={index}>{entry}</div>)
-		);
-	}
-
-	renderStudentInfo(user) {
-		return null;
-=======
-	renderThreads(threads) {
-		return threads.map((entry, index) => {
-			return (<div key={index} className="thread">{entry}</div>)
-		})
->>>>>>> master
-	}
-
 	renderThreads(threads) {
 		return threads.map((entry, index) =>{
 			return (<div key = {index} className ="thread">{entry}</div>)
@@ -44,33 +26,17 @@ class Example extends Component {
 			<div className="Example">
 				<h1 className="name">{this.props.user.name}</h1>
 				<div className="entry">Major: {this.props.user.major}</div>
-<<<<<<< HEAD
+
 				{this.isFloopMajor(this.props.user) &&
-					<div className = "threads">{this.renderThreads((this.props.user.threads))}</div>}
-=======
-				{this.isFloopMajor(this.props.user) && 
 					<div className="threads">{this.renderThreads(this.props.user.threads)}</div>}
->>>>>>> master
 			</div>
 		);
 	}
 }
 // {isFloopMajor() }
 const mapStateToProps = state => ({
-<<<<<<< HEAD
-	user: {
-		name: "Floopy",
-		major: "Floopanomics",
-		gpa: 3.5,
-		year: "Freshman",
-		interests: ["Blarg", "Honk", "Floop"],
-		threads: ['Schnops', 'Blip'],
-		graduation_date: "2",
-		looking_for: "Internship",
-	},
-=======
+
 	user: state.user
->>>>>>> master
 });
 
 export default connect(mapStateToProps)(Example);
--- src/containers/RecruiterProfile.js
+++ src/containers/RecruiterProfile.js
@@ -5,15 +5,38 @@ import './RecruiterProfile.css';
 import ErrorInput from '../components/ErrorInput';
 
 import { connect } from 'react-redux'
-
+import { bindActionCreators} from 'redux'
+import {setUser} from '../redux/actions'
+import EditableInfo from '../components/EditableInfo'
 
 class RecruiterProfile extends Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			bool: false,
+			isEditing: false,
+			edits: {},
 		}
+	}
+
+	makeEdit(key, value) {
+		const edits = {...this.state.edits, [key]: value};
+		this.setState({ edits: edits });
+	}
+
+	cancelEdit() {
+		this.setState({isEditing: false, edits: this.props.user});
+	}
+
+	//TODO should be a fetch call up to the server and update redux with the response
+	saveEdits() {
+		this.props.setUser(this.state.edits);
+		this.setState({isEditing: false});
+	}
+	componentDidMount() {
+
+		this.setState({edits: this.props.user});
+
 	}
 
 	renderInterests(interests) {
@@ -28,26 +51,39 @@ class RecruiterProfile extends Component {
 
 	render() {
 		return (
-			<div className="RecruiterProfile" onClick={() => this.setState({bool: !this.state.bool})}>
-				<h1 className="name">{this.props.user.name}</h1>
-				<div className="entry">Major: {this.props.user.major}</div>
-				<div className="entry">{this.props.user.gpa}</div>
-				<div className="entry">{this.props.user.year}</div>
-				<div className="entry">{this.renderInterests(this.props.user.interests)}</div>
-				<div className={classNames("foo", {bar: this.state.bool})}>AAAAAAAAAARG</div>
-				<ErrorInput text="hello" show={this.state.bool}/>
+			<div className="RecruiterProfile">
+				{!this.state.isEditing && <div className="btn" onClick={() => this.setState({isEditing: true})}>Edit</div>}
+				{this.state.isEditing && <div className="edit-buttons">
+					<div className="edit-buttons__save btn" onClick={() => this.saveEdits()}>Save</div>
+					<div className="edit-buttons__cancel btn" onClick={() => this.cancelEdit()}>Cancel</div>
+				</div>}
+				<EditableInfo
+					edit={this.state.isEditing}
+					placeholder="Name"
+					text={this.state.edits.name}
+					errorCode={null}
+					onChange={e => this.makeEdit('name', e)}>
+					<h1 className="name">{this.props.user.name}</h1>
+				</EditableInfo>
 			</div>
 		);
 	}
 }
+
+
+	// <h1 className="name">{this.props.user.name}</h1>
+	// <div className="entry">Major: {this.props.user.major}</div>
+	// <div className="entry">{this.props.user.gpa}</div>
+	// <div className="entry">{this.props.user.year}</div>
+	// <div className="entry">{this.renderInterests(this.props.user.interests)}</div>
+	// <div className={classNames("foo", {bar: this.state.bool})}>AAAAAAAAAARG</div>
+	// <ErrorInput text="hello" show={this.state.bool}/>
 				// {this.state.bool && <div className="baz">Hello</div>}
 
 const mapStateToProps = state => ({
-	user: {
-		name: "Floopy",
-		company_id: "3334544",
-        title: ""
-	},
+	user: state.user
 });
 
-export default connect(mapStateToProps)(RecruiterProfile);
+const mapDispatchToProps = dispatch =>
+	bindActionCreators({setUser}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(RecruiterProfile);
