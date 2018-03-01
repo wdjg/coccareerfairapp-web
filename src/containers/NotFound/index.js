d90dabc49d5144da2320b63fe4bb910640ee97ba
wiqasnassar <wiqas.nassar@gmail.com>
2018-02-28 22:36:46 -0500

    Refractored NotFound.js

--- src/containers/NotFound/index.js
+++ src/containers/NotFound/index.js
@@ -6,7 +6,7 @@ class NotFound extends Component {
 	constructor(props) {
 		super(props);
 		this.images = ['https://i.imgur.com/8srChE2.gif', 'https://i.redd.it/uwvjph19mltz.jpg', 'https://i.imgur.com/lWAYMnS.gif'];
-		this.randImage = this.images[Math.floor(Math.random()*this.images.length)];
+		this.rand_image = this.images[Math.floor(Math.random()*this.images.length)];
 	}
 	componentDidMount() {
 		// this.randImage = this.images[Math.floor(Math.random()*this.images.length)];
@@ -17,7 +17,7 @@ class NotFound extends Component {
 			<div className="NotFound">
 				<h2>The page that you're looking for doesn't exist ;__;</h2>
 				<div className="image">
-					<img src={this.randImage} alt=""/>
+					<img src={this.rand_image} alt=""/>
 				</div>
 			</div>
 		);
