bc7cf9163461b0c77e505e34ec1136cd7f2c5a0e
RoboticWater <jakebritti@gmail.com>
2018-04-13 14:50:17 -0400

    maybe fixed compilation issues

--- razzle.config.js
+++ razzle.config.js
@@ -30,22 +30,37 @@ module.exports = {
     //     name: 'static/media/[name].[hash:8].[ext]',
     //   },
     // });
-    // console.log(util.inspect(appConfig.module.rules, {showHidden: false, depth: null, showProxy: true}))
+    console.log(dev, target)
+    console.log(util.inspect(appConfig.module.rules, {showHidden: false, depth: null, showProxy: true}))
     if (target == 'web' && !dev)
-      appConfig.module.rules[4].use = ExtractTextPlugin.extract({
-      fallback: require.resolve('style-loader'),
-      use: [
-        {
-          loader: require.resolve('css-loader'),
-          options: {
-            importLoaders: 1,
-            minimize: !dev,
-          },
-        },
-        'url-loader',
-        'postcss-loader',
-      ],
-    });
+      appConfig.module.rules[4].use[3].options.plugins = () => [
+        postcssImport(),
+        nested(),
+        vars(),
+        require('postcss-flexbugs-fixes'),
+        autoprefixer({
+          browsers: [
+            '>1%',
+            'last 4 versions',
+            'Firefox ESR',
+            'not ie < 9', // React doesn't support IE8 anyway
+          ],
+          flexbox: 'no-2009',
+        }),];
+    //   appConfig.module.rules[4].use = ExtractTextPlugin.extract({
+    //   fallback: require.resolve('style-loader'),
+    //   use: [
+    //     {
+    //       loader: require.resolve('css-loader'),
+    //       options: {
+    //         importLoaders: 1,
+    //         minimize: !dev,
+    //       },
+    //     },
+    //     'url-loader',
+    //     'postcss-loader',
+    //   ],
+    // });
     // appConfig.plugins = [...appConfig.plugins, new ExtractTextPlugin({
     //    filename: 'static/css/bundle.[contenthash:8].css',
     //    id: 1,
